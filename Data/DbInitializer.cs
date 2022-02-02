53b620180ae121c55f7d0a1fdcff5bb5ac9261a3
Hugo Anes <anesfh@gmail.com>
2022-02-02 19:01:00 +0000

    Atualizado

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,44 @@
+﻿using ProjetoDeSia.Models;
+using System.Linq;
+
+namespace ProjetoDeSia.Data
+{
+    public class DbInitializer
+    {
+        //assegura que a base de dados é criada, se esta não existir ainda.
+        public static void Initialize(ProjetoDeSiaContext context)
+        {
+            context.Database.EnsureCreated();
+
+
+            //assegura que o admin master é criado logo quando a BD é criada tb
+            if (context.Utilizador.Any())
+            {
+                return;
+            }
+            else
+            {
+
+                var admin = new Utilizador[]
+                {
+                    new Utilizador
+                    {
+                        UserName = "admin",
+                        Email = "admin@gmail.com",
+                        Password = "12345",
+                        Categoria = -1
+                    }
+
+                };
+
+                foreach (Utilizador a in admin)
+                {
+                    context.Utilizador.Add(a);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        
+    }
+}
